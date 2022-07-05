3020f810882b2e3680074324f55fb3ee16885025
Jose Pablo Obando <jose.obando@gorillalogic.com>
2022-07-05 15:51:58 -0600

    Examples: pages, routing, validation, layouts, styles

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Single user, param: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.id);
+    }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Please enter an id and click the button</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        onLoadUser() {
+            this.$router.push(`/users/${this.userId}`)
+        }
+    }
+}
+</script>
